3e07891e398c98725aacef38c7cc170ab84cb999
atharv1345 <105710340+atharv1345@users.noreply.github.com>
2025-10-24 19:23:48 +0000

    changes

--- node.js/02_datatypes.js
+++ node.js/02_datatypes.js
@@ -1,10 +1,37 @@
 "use strict"; // treat all JS code as newer version
-
+// alert("hello world");
+// console.log("hello world");
 // alert(4+45) // we are using a nodejs ,not brower
 
 console.log(4+654);
 // code readability should be high
 console.log("atharv");
+// datatypes in javascript
+
+// primitive datatypes  
+// 1. number
+// 2. bigint
+// 3. string
+// 4. boolean
+// 5. null
+// 6. undefined
+// 7. symbol
+// 8. any other
+// 9. never
+// 10. void
+// 11. unknown
+// 12. object
+// 13. array
+// 14. function
+// 15. date
+// 16. anything else
+
+// non-primitive datatypes
+// 1. object
+// 2. array
+// 3. function
+// 4. date
+// 5. anything else
 
 let name ="atharv"
 let age = 18
